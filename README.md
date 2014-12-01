a4b3c30799c0c7200c07bb3093275e8275395255
Alexander Shabanov <avshabanov@gmail.com>
2014-12-01 01:07:16 -0800

    Enabling Freemarker instead of JSP (also redundant jetty libraries removed)

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ Servlet/SpringMVC demo web application written mostly in Kotlin
 
 * Set working dir to kot-website dir
 
+## Pages
+
+
+Static: ``http://127.0.0.1:8080/index.html``
+Freemarker: ``http://127.0.0.1:8080/hello.html``
--- kot-website/src/main/kotlin/com/alexshabanov/kotwebdemo/web/controllers.kt
+++ kot-website/src/main/kotlin/com/alexshabanov/kotwebdemo/web/controllers.kt
@@ -5,18 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping as req
 import org.springframework.web.bind.annotation.RequestParam as param
 import javax.servlet.http.HttpServletResponse
 import java.io.OutputStreamWriter
+import org.springframework.ui.Model
+import org.springframework.web.servlet.ModelAndView
 
 //
 // Spring MVC controllers
 //
 
 controller class PublicController {
-  req(array("/test.html")) fun index(): String = "test"
+  req(array("/test.html")) fun test(): String = "test"
 
-  req(array("/hello.html")) fun hello(r: HttpServletResponse,
-                                      param("greeting", defaultValue = "Hello") greeting: String) {
-    OutputStreamWriter(r.getOutputStream()).use {
-      it.append("<p>" + greeting + " from Spring!</p>")
-    }
-  }
+  req(array("/hello.html")) fun hello(param("greeting", defaultValue = "Hello") greeting: String): ModelAndView =
+      ModelAndView("hello", "greeting", greeting)
 }
