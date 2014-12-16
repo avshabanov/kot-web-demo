e3342ac3ce66271c44f6ddf8eb256e7d8d63737a
Alexander Shabanov <avshabanov@gmail.com>
2014-12-15 22:58:45 -0800

    Update

--- kot-website/src/main/kotlin/com/alexshabanov/kotwebdemo/web/controllers.kt
+++ kot-website/src/main/kotlin/com/alexshabanov/kotwebdemo/web/controllers.kt
@@ -2,7 +2,7 @@ package com.alexshabanov.kotwebdemo.web.controllers
 
 import org.springframework.stereotype.Controller as controller
 import org.springframework.web.bind.annotation.RequestMapping as req
-import org.springframework.web.bind.annotation.RequestParam as param
+import org.springframework.web.bind.annotation.RequestParam as par
 import org.springframework.web.bind.annotation.ResponseBody as respBody
 import javax.servlet.http.HttpServletResponse
 import java.io.OutputStreamWriter
@@ -18,15 +18,17 @@ import com.alexshabanov.kotwebdemo.service.UserProfile
 // Spring MVC controllers
 //
 
+/** Public HTML controller. */
 req(array("/g")) controller class PublicController {
   req(array("/index")) fun test(): String = "index"
 
-  req(array("/hello")) fun hello(param("greeting", defaultValue = "Hello") greeting: String): ModelAndView =
+  req(array("/hello")) fun hello(par("greeting", defaultValue = "Hello") greeting: String): ModelAndView =
       ModelAndView("hello", "greeting", greeting)
 }
 
+/** REST API controller. */
 req(array("/rest")) controller class RestController(val userService: UserService) {
 
-  req(array("/user")) respBody fun getUserProfiles(param("id") ids: Array<Long>): GetUserProfilesResponse =
+  req(array("/user")) respBody fun getUserProfiles(par("id") ids: Array<Long>): GetUserProfilesResponse =
       userService.getUserProfiles(GetUserProfilesRequest(userIds = ids.toArrayList())).get()
 }
